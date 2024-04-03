78da51b24243db265e29ee39f74ff5990a2b4aa1
Andre <aruiz_220000001163@uic.edu.ph>
2024-04-03 17:54:34 +0800

    Review, MyPatients and Icons

--- src/router/routers.jsx
+++ src/router/routers.jsx
@@ -7,6 +7,9 @@ import Login from "pages/Login"
 import MyPatients from "pages/MyPatients"
 import Footer from "components/Footer"
 import Reportform from "pages/Reportform"
+import Home from "pages/Home"
+import Review from "pages/Review"
+
 
 function routers() {
 
@@ -34,6 +37,14 @@ function routers() {
         {
           path:"/reportform",
           element: <Reportform/>
+        },
+        {
+          path:"/home",
+          element: <Home/>
+        },
+        {
+          path:"/review",
+          element: <Review/>
         }
       ]
     }
